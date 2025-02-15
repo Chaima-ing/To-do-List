ce1ed2ea8cb2b1eb8101dbb05665ec5277fd7bc6
chaima <chaimaa.djekhrab@gmail.com>
2025-02-15 15:36:35 +0100

    style input box

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -11,9 +11,9 @@ const ToDo = () => {
       </div>
 
       {/* input box */}
-       <div>
-          <input type="text" placeholder='Add your task'/>
-          <button>ADD +</button>
+       <div className='flex items-center my-7 bg-gray-200 rounded-full'>
+          <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task'/>
+          <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
        </div>
     </div>
   )
